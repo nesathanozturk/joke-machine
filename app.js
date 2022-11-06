6ac54840a90bd1d74b2691bb7170dd8919d2e7b9
Neşathan Öztürk <macbookpro@MacBook-Pro.local>
2022-11-06 15:21:07 +0300

    created  elements

---
+++ app.js
@@ -0,0 +1,8 @@
+// ELEMENTS
+const jokeQuestion = document.getElementById("joke_question");
+const jokeAnswer = document.getElementById("joke_answer");
+const generateBtn = document.getElementById("generateBtn");
+
+// FUNCTION:
+generateBtn.addEventListener("click", generateJoke);
+generateJoke();
