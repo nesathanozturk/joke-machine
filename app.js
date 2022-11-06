3e5f094d727a609b32325b1911ec0fd5d61a922e
Neşathan Öztürk <macbookpro@MacBook-Pro.local>
2022-11-06 15:28:22 +0300

    added api

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const jokeQuestion = document.getElementById("joke_question");
 const jokeAnswer = document.getElementById("joke_answer");
 const generateBtn = document.getElementById("generateBtn");
 
-// FUNCTION:
+// FUNCTIONS:
 generateBtn.addEventListener("click", generateJoke);
 generateJoke();
+
+function generateJoke() {
+    fetch("https://official-joke-api.appspot.com/random_joke")
+    .then((res) => res.json())
+    .then((data) => {
+        jokeQuestion.innerHTML = data.setup;
+        jokeAnswer.innerHTML = data.punchline;
+    });
+};
