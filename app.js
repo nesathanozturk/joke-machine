ae0c9702a7de506be516aa35dd2a1133ceee3ee3
Neşathan Öztürk <107864503+nesathanozturk@users.noreply.github.com>
2022-12-24 22:24:51 +0300

    refactor: the codes are organized

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const jokeQuestion = document.getElementById("joke_question");
 const jokeAnswer = document.getElementById("joke_answer");
 const generateBtn = document.getElementById("generateBtn");
 
+// API
+const BASE_URL = "https://official-joke-api.appspot.com/random_joke";
+
 // FUNCTION:
 generateJoke();
 
 generateBtn.addEventListener("click", generateJoke);
+
 async function generateJoke() {
-    const JOKE_API = await fetch("https://official-joke-api.appspot.com/random_joke");
-    const jokes = await JOKE_API.json();
-    jokeQuestion.innerHTML = jokes.setup;
-    jokeAnswer.innerHTML = jokes.punchline;
+  const res = await fetch(BASE_URL);
+  const data = await res.json();
+  jokeQuestion.innerHTML = data.setup;
+  jokeAnswer.innerHTML = data.punchline;
 };
